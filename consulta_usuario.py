8a878321c053c9d1024f881c9b4d9dbf5ee1c591
TomiBeads <jesusantoniomaganacastillo7@gmail.com>
2025-08-11 10:35:06 -0600

    2da actializacion de consulta usuario

--- consulta_usuario.py
+++ consulta_usuario.py
@@ -1,38 +1,50 @@
+# consulta_usuario.py
 import flet as ft
 import airtable as at
+import principal as pr  # importa el archivo que contiene tu menú principal
 
 def main(page: ft.Page):
     page.title = "Consulta"
     page.theme_mode = "light"
-    page.window.height = 600
-    page.window.width = 800
+    page.window.width = 500
+    page.window.height = 650
     page.appbar = ft.AppBar(
-        title=ft.Text("Consulta de los usuarios en la nube"),
+        title=ft.Text("Consulta de Usuario en la Nube"),
         leading=ft.Icon("cloud"),
         center_title=True,
-        bgcolor="green",
+        bgcolor=ft.Colors.BLUE_900,
         color="white"
     )
-    #Tabla de usuarios
+
     encabezado = [
         ft.DataColumn(ft.Text("Clave")),
         ft.DataColumn(ft.Text("Contraseña")),
-        ft.DataColumn(ft.Text("Nombre completo")),
-        ft.DataColumn(ft.Text("Es administrador"))
+        ft.DataColumn(ft.Text("Nombre Completo")),
+        ft.DataColumn(ft.Text("Es Administrador"))
     ]
+
     filas = []
-    datos = at.Usuario.all()
+    datos = at.usuario.all()
     for d in datos:
-        celda = ft.DataCell(ft.Text(d.clave))
+        celda1 = ft.DataCell(ft.Text(d.clave))
         celda2 = ft.DataCell(ft.Text(d.contra, color="white", selectable=True))
         celda3 = ft.DataCell(ft.Text(d.nombre))
         celda4 = ft.DataCell(ft.Text(d.admin))
-        fila = ft.DataRow([celda, celda2, celda3, celda4])
+        fila = ft.DataRow([celda1, celda2, celda3, celda4])
         filas.append(fila)
+
     tbl_usuarios = ft.DataTable(encabezado, filas)
-    
-    page.add(tbl_usuarios)
+
+    def regresar_al_menu(e: ft.ControlEvent):
+        page.clean()
+        pr.main(page)  # vuelve a la página principal
+
+    btn_regresar = ft.ElevatedButton("Regresar",color= "White", icon= "ARROW_BACK",icon_color="white", bgcolor= "BLACK12", on_click=regresar_al_menu)
+
+    page.clean()
+    page.add(btn_regresar, tbl_usuarios)
     page.update()
 
+
 if __name__ == "__main__":
-    ft.app(target=main)
+    ft.app(target=main, view=ft.AppView.WEB_BROWSER)
