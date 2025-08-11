7a1cbfd9f0b49f7655a824565e5bc749dd4aa6e9
TomiBeads <jesusantoniomaganacastillo7@gmail.com>
2025-08-11 10:39:22 -0600

    actualizacion del codigo

--- airtable.py
+++ airtable.py
@@ -1,12 +1,3 @@
-#from pyairtable import Api
-
-#api = Api("patf0NZCu1R0XOVDs.ac01eb0dc160ee42dacdafd5e70b6a06bab44fed6b3c7171befe7e80f4d29e39")
-#tabla = api.table("appNDA3zjKznZwEsr", "usuario")
-
-#registros = tabla.all()
-#for r in registros:
-    #print(r["fields"])
-
 from pyairtable.orm import Model
 from pyairtable.orm import fields
 
@@ -16,7 +7,7 @@ class Usuario (Model):
     nombre = fields.TextField("nombre")
     admin = fields.CheckboxField("admin")
     class Meta:
-        api_key = "patf0NZCu1R0XOVDs.ac01eb0dc160ee42dacdafd5e70b6a06bab44fed6b3c7171befe7e80f4d29e39"
+        api_key = "patn1DWLGpKowibsv.39bdd2ccad950be7954beb56934cc11958fe8d38c9972501c9d53818cf43b8d0"
         base_id = "appNDA3zjKznZwEsr"
         table_name = "Usuario"
 
@@ -30,26 +21,6 @@ class bioenergia(Model):
     latitud = fields.FloatField("latitud")
     longitud = fields.FloatField("longitud")
     class Meta:
-        api_key = "patf0NZCu1R0XOVDs.ac01eb0dc160ee42dacdafd5e70b6a06bab44fed6b3c7171befe7e80f4d29e39"
+        api_key = "patn1DWLGpKowibsv.39bdd2ccad950be7954beb56934cc11958fe8d38c9972501c9d53818cf43b8d0"
         base_id = "appNDA3zjKznZwEsr"
         table_name = "bioenergia"
-
-#cacao = bioenergia(
-    #cultivo = "Cacao",
-    #parte = "Cascara",
-    #municipio = "Cunduacan",
-    #cantidad = 40.3,
-    #area = 20.4,
-    #energia = 18.2,
-    #latitud = 18.076169,
-    #longitud = 10.076169
-#)
-
-#cacao.save()
-#nuevo = Usuario(
-    #clave = "222H16010",
-    #contra = "Magaña",
-    #nombre = "Señor Magaña",
-    #admin = 0
-#)
-#nuevo.save()
